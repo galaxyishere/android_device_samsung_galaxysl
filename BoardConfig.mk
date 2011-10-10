ec8420b53d18f3e2c177d9401d7a27198ef609aa
codeworkx <daniel.hillenbrand@codeworkx.de>
2011-10-10 17:55:45 +0200

    BoardConfig: cleanup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,16 +1,9 @@
 # inherit from the proprietary version
 -include vendor/samsung/galaxysl/BoardConfigVendor.mk
 
-#OMAP_ENHANCEMENT := true
-ifdef OMAP_ENHANCEMENT
-COMMON_GLOBAL_CFLAGS += -DOMAP_ENHANCEMENT
-endif
-
-TARGET_BOARD_PLATFORM := latona
-TARGET_BOOTLOADER_BOARD_NAME := GT-I9003
-
 # Board properties
 TARGET_BOARD_PLATFORM := omap3
+TARGET_BOOTLOADER_BOARD_NAME := GT-I9003
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
@@ -24,6 +17,20 @@ COMMON_GLOBAL_CFLAGS += -DTARGET_OMAP3
 
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
+
+BOARD_USES_COMBINED_RECOVERY := true
+BOARD_PROVIDES_BOOTMODE := true
+TARGET_PROVIDES_INIT := true
+TARGET_PROVIDES_INIT_TARGET_RC := true
+TARGET_RECOVERY_INITRC := device/samsung/galaxysl/recovery.rc
+
+# recovery
+BOARD_BML_BOOT := /dev/block/bml7
+BOARD_BML_RECOVERY := /dev/block/bml7
+BOARD_USES_MMCUTILS := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_CUSTOM_RECOVERY_KEYMAPPING:= ../../device/samsung/galaxysl/recovery/recovery_ui.c
 
 BOARD_NAND_PAGE_SIZE := 4096 -s 128
 BOARD_KERNEL_PAGESIZE := 4096
@@ -39,55 +46,31 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 339738624
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 2013200384
 BOARD_FLASH_BLOCK_SIZE := 4096
-
-# recovery
-BOARD_BML_BOOT := /dev/block/bml7
-BOARD_BML_RECOVERY := /dev/block/bml7
-BOARD_USES_MMCUTILS := true
-BOARD_HAS_NO_MISC_PARTITION := true
-BOARD_HAS_NO_SELECT_BUTTON := true
-
-#TARGET_TOOLS_PREFIX := /media/android/toolchain/arm-2011.03/bin/arm-none-eabi-
-#TARGET_TOOLS_PREFIX := /media/android/toolchain/arm-2009q3/bin/arm-none-eabi-
-
-TARGET_PREBUILT_KERNEL := device/samsung/galaxysl/kernel
 TARGET_USERIMAGES_USE_EXT4 := true
-
-# deprecated
-BOARD_USES_COMBINED_RECOVERY := true
-BOARD_PROVIDES_BOOTMODE := true
-
-TARGET_PROVIDES_INIT := true
-TARGET_PROVIDES_INIT_TARGET_RC := true
-TARGET_RECOVERY_INITRC := device/samsung/galaxysl/recovery.rc
-BOARD_CUSTOM_RECOVERY_KEYMAPPING:= ../../device/samsung/galaxysl/recovery/recovery_ui.c
+BOARD_HAS_LARGE_FILESYSTEM := true
 
 BOARD_USE_USB_MASS_STORAGE_SWITCH := true
 BOARD_UMS_LUNFILE := "/sys/devices/platform/usb_mass_storage/lun0/file"
 
-# deprecated
-BOARD_HAS_DOWNLOAD_MODE := true
-
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
-#BOARD_HAVE_BLUETOOTH_BCM := true
 
+# Egl
 BOARD_EGL_CFG := device/samsung/galaxysl/egl.cfg
 
 # Audio
 BOARD_USES_GENERIC_AUDIO := false
-
 BOARD_USES_ALSA_AUDIO := true
-#BUILD_WITH_ALSA_UTILS := true
-#TARGET_PROVIDES_LIBAUDIO := true
+BUILD_WITH_ALSA_UTILS := true
 ALSA_DEFAULT_SAMPLE_RATE := 44100
 
-# MultiMedia defines
+# Camera
 USE_CAMERA_STUB := false
-
 BOARD_CAMERA_LIBRARIES := libcamera
+#BOARD_USE_FROYO_LIBCAMERA := true
 BOARD_SECOND_CAMERA_DEVICE := true
 
+# Video
 HARDWARE_OMX := true
 ifdef HARDWARE_OMX
 OMX_JPEG := true
@@ -101,8 +84,6 @@ BOARD_OPENCORE_LIBRARIES := libOMX_Core
 BOARD_OPENCORE_FLAGS := -DHARDWARE_OMX=1
 BOARD_USES_TI_CAMERA_HAL := true
 endif
-
-#BOARD_USE_FROYO_LIBCAMERA := true
 
 # FM Radio
 BOARD_HAVE_FM_RADIO := true
@@ -120,11 +101,5 @@ WIFI_DRIVER_MODULE_PATH     := "/system/etc/wifi/tiwlan_drv.ko"
 WIFI_DRIVER_MODULE_NAME     := "tiwlan_drv"
 WIFI_FIRMWARE_LOADER        := "wlan_loader"
 AP_CONFIG_DRIVER_WILINK     := true
-
-# Use this flag if the board has a ext4 partition larger than 2gb
-BOARD_HAS_LARGE_FILESYSTEM := true
-
-WITH_JIT := true
-ENABLE_JSC_JIT := true
 
 TARGET_OTA_ASSERT_DEVICE := GT-I9003
