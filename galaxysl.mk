140ce8b811ebef8c164800f386fb144a7d0f69f8
codeworkx <daniel.hillenbrand@codeworkx.de>
2011-10-18 22:30:38 +0200

    fix typo

--- galaxysl.mk
+++ galaxysl.mk
@@ -27,7 +27,7 @@ PRODUCT_COPY_FILES += \
 	device/samsung/galaxysl/lpm.rc:root/lpm.rc \
 	device/samsung/galaxysl/lpm.rc:recovery/root/lpm.rc \
 	device/samsung/galaxysl/ueventd.rc:root/ueventd.rc \
-	device/samsung/galaxysl/ueventd.latona.rc:root/ueventd.latona.rc
+	device/samsung/galaxysl/ueventd.latona.rc:root/ueventd.latona.rc \
 	device/samsung/galaxysl/ueventd.rc:recovery/root/ueventd.rc \
 	device/samsung/galaxysl/ueventd.latona.rc:recovery/root/ueventd.latona.rc
 
